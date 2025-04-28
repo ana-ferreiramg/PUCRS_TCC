81b1eae75b277340d40217bd0e50ec7210acc1f8
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-28 08:49:19 -0300

    feat: create DTOs for companies module

--- api/src/modules/companies/dto/create-company.dto.ts
+++ api/src/modules/companies/dto/create-company.dto.ts
@@ -1,1 +1,42 @@
-export class CreateCompanyDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateCompanyDto {
+  @IsString()
+  @IsNotEmpty({ message: 'O nome é obrigatório e não pode estar vazio.' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres.' })
+  @MaxLength(100, { message: 'O nome pode ter no máximo 100 caracteres.' })
+  name: string;
+
+  @IsEmail({}, { message: 'O e-mail fornecido não é válido.' })
+  @IsNotEmpty({ message: 'O e-mail é obrigatório e não pode estar vazio.' })
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^\d{2}\d{8}$/, {
+    message:
+      'O número de telefone deve conter apenas o DDD e o número (ex: 1199999999)',
+  })
+  @Transform(({ value }) => (value ? `+55${value}` : value)) // Adiciona o código do país (+55) automaticamente
+  phone?: string;
+
+  @IsString()
+  @MaxLength(255, {
+    message: 'O endereço não pode ter mais de 255 caracteres.',
+  })
+  @Matches(/^[A-Za-z0-9\s,.-áãçéíóúàèù]+$/, {
+    message:
+      'O endereço deve seguir o formato: "Rua XYZ, 123, Bairro ABC, Cidade DEF, Estado GH, 12345-678"',
+  })
+  @Transform(({ value }) => (value ? value.trim() : value))
+  address: string;
+}
--- api/src/modules/companies/dto/update-company.dto.ts
+++ api/src/modules/companies/dto/update-company.dto.ts
@@ -1,4 +1,44 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { CreateCompanyDto } from './create-company.dto';
 
-export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
+export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
+  @IsOptional()
+  @IsString()
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres.' })
+  @MaxLength(100, { message: 'O nome pode ter no máximo 100 caracteres.' })
+  name?: string;
+
+  @IsOptional()
+  @IsEmail({}, { message: 'O e-mail fornecido não é válido.' })
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^\d{2}\d{8}$/, {
+    message:
+      'O número de telefone deve conter apenas o DDD e o número (ex: 1199999999)',
+  })
+  @Transform(({ value }) => (value ? `+55${value}` : value)) // Adiciona o código do país (+55) automaticamente
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255, {
+    message: 'O endereço não pode ter mais de 255 caracteres.',
+  })
+  @Matches(/^[A-Za-z0-9\s,.-áãçéíóúàèù]+$/, {
+    message:
+      'O endereço deve seguir o formato: "Rua XYZ, 123, Bairro ABC, Cidade DEF, Estado GH, 12345-678"',
+  })
+  @Transform(({ value }) => (value ? value.trim() : value))
+  address?: string;
+}
