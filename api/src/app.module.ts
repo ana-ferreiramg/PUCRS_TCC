80fedc4fd49c4f712069c8693d386f2a8157eae8
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-28 08:28:01 -0300

    feat(companies): create companies module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { UsersModule } from '@modules/users/users.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@shared/config/config.module';
+import { CompaniesModule } from './modules/companies/companies.module';
 
 @Module({
-  imports: [ConfigModule, UsersModule],
+  imports: [ConfigModule, UsersModule, CompaniesModule],
   controllers: [],
   providers: [],
 })
