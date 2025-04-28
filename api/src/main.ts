fd24c69e37dc2f7f0d4c5ccce20f29e83d3ec2ff
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-28 08:05:14 -0300

    refactor: update import path for ConfigService to use module alias

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@shared/config/config.service';
 import { AppModule } from './app.module';
-import { ConfigService } from './shared/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
