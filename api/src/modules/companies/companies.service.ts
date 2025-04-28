cb2d5ae507421ae49229ed3b9ec1eb8ed2c0a7f6
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-28 09:26:07 -0300

    feat(companies): add create function to handle company creation

--- api/src/modules/companies/companies.service.ts
+++ api/src/modules/companies/companies.service.ts
@@ -1,11 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
+import { Company } from '@prisma/client';
+import { CompaniesRepository } from '@shared/database/repositories/companies.repositories';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Injectable()
 export class CompaniesService {
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+  constructor(private readonly companiesRepo: CompaniesRepository) {}
+
+  async create(createCompanyDto: CreateCompanyDto): Promise<Company> {
+    const { name, email, phone, address } = createCompanyDto;
+
+    const emailTaken = await this.companiesRepo.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('Este email já está sendo usado.');
+    }
+
+    let slug = slugify(name, {
+      lower: true,
+      strict: true,
+    });
+
+    let slugTaken = await this.companiesRepo.findUnique({
+      where: { slug },
+      select: { id: true },
+    });
+
+    // Caso o slug já exista, adicione um número incremental para garantir unicidade
+    let counter = 1;
+    while (slugTaken) {
+      const newSlug = `${slug}-${counter}`;
+      slugTaken = await this.companiesRepo.findUnique({
+        where: { slug: newSlug },
+        select: { id: true },
+      });
+      if (!slugTaken) {
+        slug = newSlug;
+      }
+      counter++;
+    }
+
+    const company = await this.companiesRepo.create({
+      data: {
+        name,
+        slug,
+        email,
+        phone,
+        address,
+      },
+    });
+
+    return company;
   }
 
   findAll() {
