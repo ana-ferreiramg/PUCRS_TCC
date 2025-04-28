f77f55c0bc5bb3516716d2b55bb0dc8b66c09d4d
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-28 08:59:22 -0300

    feat: add CompaniesRepository to companies module

--- api/src/modules/companies/companies.module.ts
+++ api/src/modules/companies/companies.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@shared/database/prisma.service';
+import { CompaniesRepository } from '@shared/database/repositories/companies.repositories';
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 
 @Module({
   controllers: [CompaniesController],
-  providers: [CompaniesService, PrismaService],
+  providers: [CompaniesService, PrismaService, CompaniesRepository],
 })
 export class CompaniesModule {}
