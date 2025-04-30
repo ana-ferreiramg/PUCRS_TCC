72056a7bf57171ce0d77d88f964828b3408697e9
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-29 21:08:41 -0300

    feat(database): provide and export repositories globally

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { CompaniesRepository } from './repositories/companies.repositories';
+import { UsersRepository } from './repositories/users.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [],
+  providers: [PrismaService, UsersRepository, CompaniesRepository],
+  exports: [PrismaService, UsersRepository, CompaniesRepository],
 })
 export class DatabaseModule {}
