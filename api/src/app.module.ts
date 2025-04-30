9b7a37ba1012f2017f95ba774b80581fb08ed8cb
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-29 21:10:08 -0300

    fix(app): ensure global repositories are available by importing DatabaseModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { UsersModule } from '@modules/users/users.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@shared/config/config.module';
+import { DatabaseModule } from '@shared/database/database.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { CompaniesModule } from './modules/companies/companies.module';
 
 @Module({
-  imports: [ConfigModule, UsersModule, CompaniesModule],
+  imports: [
+    DatabaseModule,
+    ConfigModule,
+    UsersModule,
+    CompaniesModule,
+    CategoriesModule,
+  ],
   controllers: [],
   providers: [],
 })
