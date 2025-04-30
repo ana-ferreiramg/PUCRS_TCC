7727b5644670b570f3ef390c8bb7a575468bdde6
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-04-29 21:06:20 -0300

    feat(main): enable global validation pipe in main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@shared/config/config.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
   const port = configService.port;
