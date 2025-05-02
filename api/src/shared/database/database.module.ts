863adec088017a2c251741aa3ff1639a84f4ad87
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-05-02 11:31:11 -0300

    feat(database): add products.repository and register it in DatabaseModule

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { CategoriesRepository } from './repositories/categories.repository';
 import { CompaniesRepository } from './repositories/companies.repositories';
+import { ProductsRepository } from './repositories/products.repository';
 import { UsersRepository } from './repositories/users.repositories';
 
 @Global()
@@ -11,12 +12,14 @@ import { UsersRepository } from './repositories/users.repositories';
     UsersRepository,
     CompaniesRepository,
     CategoriesRepository,
+    ProductsRepository,
   ],
   exports: [
     PrismaService,
     UsersRepository,
     CompaniesRepository,
     CategoriesRepository,
+    ProductsRepository,
   ],
 })
 export class DatabaseModule {}
