723f59aae8edddd12b89e1297cf0a70ea6bca639
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-05-03 14:53:17 -0300

    feat(products): add necessary services to products module

--- api/src/modules/products/products.module.ts
+++ api/src/modules/products/products.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@shared/config/config.module';
+import { FileService } from '@shared/utils/file.service';
+import { ImageService } from '@shared/utils/image.service';
+import { ImgurService } from '@shared/utils/imgur.service';
+import { SharpService } from '@shared/utils/sharp.service';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [
+    ProductsService,
+    ImgurService,
+    SharpService,
+    FileService,
+    ImageService,
+  ],
 })
 export class ProductsModule {}
