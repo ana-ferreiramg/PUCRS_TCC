d10b7ccd7e5c6cc32fcdb8033e2aa4c599203965
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-06-18 19:35:31 -0300

    chore(app): import and register AuthModule in AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@shared/config/config.module';
 import { DatabaseModule } from '@shared/database/database.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { CompaniesModule } from './modules/companies/companies.module';
-import { ProductsModule } from './modules/products/products.module';
-import { OrdersModule } from './modules/orders/orders.module';
 import { OrderItemsModule } from './modules/order-items/order-items.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { ProductsModule } from './modules/products/products.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { OrderItemsModule } from './modules/order-items/order-items.module';
     ProductsModule,
     OrdersModule,
     OrderItemsModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
